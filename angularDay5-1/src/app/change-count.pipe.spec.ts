1b7852559f2056407905bb5f48008ed2d0f811a1
tsnm-x <84290898+tsnm-x@users.noreply.github.com>
2022-03-01 15:36:29 +0200

    Delete angularDay5-1 directory

--- angularDay5-1/src/app/change-count.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ChangeCountPipe } from './change-count.pipe';
-
-describe('ChangeCountPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ChangeCountPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- angularDay5-1/src/app/price-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PricePipePipe } from './price-pipe.pipe';
-
-describe('PricePipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PricePipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
